bd40179c6ea03b788e67e6d0a45a50e94ed82976
James Oliver <joliver@rancher.com>
2016-03-30 13:19:39 -0700

    asdf

--- templates/triplea/0/image/run.sh
+++ templates/triplea/0/image/run.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-
-# may need adjustment
-MAX_HEAP=256
-PORT=3304
-
-# we shouldn't have to to change this
 INDEX=$(curl rancher-metadata/2015-12-19/self/container/service_index)
 LOBBY_HOST=tripleawarclub.org
 LOBBY_PORT=3303
 
 java \
   -server \
-  -Xmx${MAX_HEAP}m \
+  -Xmx${MAX_HEAP} \
   -Djava.awt.headless=true \
   -classpath /triplea/bin/triplea.jar \
   games.strategy.engine.framework.headlessGameServer.HeadlessGameServer \
@@ -25,5 +19,5 @@ java \
     triplea.lobby.port=${LOBBY_PORT} \
     triplea.name=Bot${INDEX}_${NAME} \
     triplea.lobby.game.hostedBy=Bot${INDEX}_${NAME} \
-    triplea.lobby.game.supportEmail=${SUPPORT_EMAIL} \
+    triplea.lobby.game.supportEmail=${EMAIL} \
     triplea.lobby.game.comments="automated_host"
